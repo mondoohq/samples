229b3165626f2960ac3e6f4757326a05ad1ae170
Manuel Weber <manuel@mondoo.com>
2023-04-18 21:54:21 +0200

    added more output
    Signed-off-by: Manuel Weber <manuel@mondoo.com>

--- gke/outputs.tf
+++ gke/outputs.tf
@@ -1,3 +1,21 @@
 output "file" {
     value = data.template_file.init.rendered
 }
+
+output "attacker_vm_name" {
+    value =  google_compute_instance.pass-n2d-res.name
+}
+
+output "summary" {
+    value = <<EOT
+
+Connect to attacker VM (in 3-4 separate terminals):
+gcloud compute ssh ${google_compute_instance.pass-n2d-res.name}
+
+export KUBECONFIG="$ {PWD}/aks-kubeconfig"
+kubectl apply -f ../assets/dvwa-deployment.yml
+kubectl port-forward $(kubectl get pods -o name) 8080:80
+
+EOT
+
+}
