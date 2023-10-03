d87919ac1ff48bcf41e7a6dfe91499bba5776da5
Manuel Weber <manuel@mondoo.com>
2023-10-03 15:08:43 +0200

    fix plan block++
    Signed-off-by: Manuel Weber <manuel@mondoo.com>

--- azure/versions.tf
+++ azure/versions.tf
@@ -34,6 +34,10 @@ terraform {
 }
 
 provider "azurerm" {
-  features {}
+  features {
+    resource_group {
+      prevent_deletion_if_contains_resources = false
+    }
+  }
   tenant_id = var.tenant_id
 }
