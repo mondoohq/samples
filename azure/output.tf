0c63ca972100cfa35e387b35f47124cded112222
Manuel Weber <manuel@mondoo.com>
2023-10-05 11:39:36 +0200

    updated output
    Signed-off-by: Manuel Weber <manuel@mondoo.com>

--- azure/output.tf
+++ azure/output.tf
@@ -11,13 +11,20 @@ output "tls_private_key" {
   sensitive = true
 }
 
+output "random_password" {
+  value = random_password.password.result
+  sensitive = true
+}
+
 output "summary" {
   value = <<EOT
 
-attacker vm public ip: ${azurerm_windows_virtual_machine.attacker_vm.public_ip_address}
+Windows VM Public IP: ${azurerm_windows_virtual_machine.attacker_vm.public_ip_address}
 
-terraform output -raw tls_private_key > id_rsa
-ssh -o StrictHostKeyChecking=no -i id_rsa azureuser@${azurerm_windows_virtual_machine.attacker_vm.public_ip_address}
+Password: ${random_password.password.result}
+
+Connection: xfreerdp /u:adminusercis /v:${azurerm_windows_virtual_machine.attacker_vm.public_ip_address}:3389 /h:1048 /w:1920 /p:'${random_password.password.result}' +clipboard
+
 
 EOT
 }
