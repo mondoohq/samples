ebbecc6cb04f29821e25fb06ad7ddce14863da88
Patrick Münch <patrick.muench1111@gmail.com>
2023-11-30 12:41:25 +0100

    ⭐️ update azure deployment (#51)
    Signed-off-by: Patrick Münch <patrick.muench1111@gmail.com>

--- azure/output.tf
+++ azure/output.tf
@@ -1,60 +1,12 @@
-output "resource_group_name" {
-  value = azurerm_resource_group.rg.name
+
+output "windows_10" {
+  value = module.windows10 == [] ? [""] : module.windows10[0].public_ip_dns_name
 }
 
-output "public_ip_address" {
-  value = module.windows11.public_ip_address
+output "windows_11" {
+  value = module.windows11 == [] ? [""] : module.windows11[0].public_ip_dns_name
 }
 
-output "random_password" {
-  value = random_password.password.result
-  sensitive = true
+output "username_password" {
+  value = "Username: ${var.windows_admin_username}, Password: ${var.windows_admin_password}"
 }
-
-output "locals" {
-  value = local.windows_user_data_cnspec
-  sensitive = true
-}
-
-output "summary" {
-  sensitive = true
-  value = <<EOT
-
-Windows VM Public IP: ${module.windows11.public_ip_address[0]}
-
-Password: ${random_password.password.result}
-
-Connection: xfreerdp /u:adminusercis /v:${module.windows11.public_ip_address[0]}:3389 /h:1048 /w:1920 /p:'${random_password.password.result}' +clipboard
-
-Debugging local.windows_user_data_cnspec:\n ${local.windows_user_data_cnspec}
-
-EOT
-}
-
-#output "resource_group_name" {
-#  value = azurerm_resource_group.rg.name
-#}
-#
-#output "public_ip_address" {
-#  value = azurerm_windows_virtual_machine.attacker_vm.public_ip_address
-#}
-#
-#output "random_password" {
-#  value = random_password.password.result
-#  sensitive = true
-#}
-#
-#output "summary" {
-#  sensitive = true
-#  value = <<EOT
-#
-#Windows VM Public IP: ${azurerm_windows_virtual_machine.attacker_vm.public_ip_address}
-#
-#Password: ${random_password.password.result}
-#
-#Connection: xfreerdp /u:adminusercis /v:${azurerm_windows_virtual_machine.attacker_vm.public_ip_address}:3389 /h:1048 /w:1920 /p:'${random_password.password.result}' +clipboard
-#
-#
-#EOT
-#}
-#
