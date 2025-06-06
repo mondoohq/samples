ce9ca8a55e44d654ddf6a71b1e7545b9650cd447
Philip Balinov <philip@mondoo.com>
2025-06-06 12:48:15 +0300

    Fix missing 18.04 and 20.04 amis
    - Point to Pro 18.04 AMI
    - Delete reference to removed CIS Ubuntu 20.04 AMI

--- aws/ec2-instances/main.tf
+++ aws/ec2-instances/main.tf
@@ -729,37 +729,6 @@ module "ubuntu2004_cnspec" {
   user_data_replace_on_change = true
 }
 
-module "ubuntu2004_cis" {
-  source  = "terraform-aws-modules/ec2-instance/aws"
-  version = "~> 5.7.1"
-
-  create                      = var.create_ubuntu2004_cis
-  name                        = "${var.prefix}-ubuntu2004-${random_id.instance_id.id}"
-  ami                         = data.aws_ami.ubuntu2004_cis.id
-  instance_type               = var.linux_instance_type
-  vpc_security_group_ids      = [module.linux_sg.security_group_id]
-  subnet_id                   = module.vpc.public_subnets[0]
-  key_name                    = var.aws_key_pair_name
-  associate_public_ip_address = true
-}
-
-module "ubuntu2004_cis_cnspec" {
-  source  = "terraform-aws-modules/ec2-instance/aws"
-  version = "~> 5.7.1"
-
-  create                      = var.create_ubuntu2004_cis_cnspec
-  name                        = "${var.prefix}-ubuntu2004-cis-cnspec-${random_id.instance_id.id}"
-  ami                         = data.aws_ami.ubuntu2004_cis.id
-  instance_type               = var.linux_instance_type
-  vpc_security_group_ids      = [module.linux_sg.security_group_id]
-  subnet_id                   = module.vpc.public_subnets[0]
-  key_name                    = var.aws_key_pair_name
-  associate_public_ip_address = true
-  user_data                   = base64encode(local.linux_user_data)
-  user_data_replace_on_change = true
-}
-
-
 
 // Ubuntu 22.04
 
