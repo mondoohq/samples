d346fbb623e394a1a0a96f85b2566b27bcc5047f
Manuel Weber <manuel@mondoo.com>
2025-10-24 11:16:25 +0200

    back using provider 5.0 for comp
    Signed-off-by: Manuel Weber <manuel@mondoo.com>

--- aws/ec2-instances/providers.tf
+++ aws/ec2-instances/providers.tf
@@ -1,18 +1,10 @@
-provider "aws" {
-  region = var.region
-
-  default_tags {
-    tags = var.default_tags
-  }
-}
-
 terraform {
   required_version = ">= 1.0"
 
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 6.0"
+      version = "~> 5.0"
     }
     http = {
       source  = "hashicorp/http"
@@ -22,5 +14,13 @@ terraform {
       source  = "hashicorp/random"
       version = "~> 3.0"
     }
+  }
+}
+
+provider "aws" {
+  region = var.region
+
+  default_tags {
+    tags = var.default_tags
   }
 }
